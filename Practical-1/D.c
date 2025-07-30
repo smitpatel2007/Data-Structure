ca56a2a73b4b8beee683b736157b6de4efeeb16d
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:18:24 +0530

    Update D.c

--- Practical-1/D.c
+++ Practical-1/D.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {15, 22, 9, 40, 30};
+    int i, min, max, sum = 0;
+    float average;
+
+    min = max = arr[0];
+
+    for(i = 0; i < 5; i++) {
+        if(arr[i] < min)
+            min = arr[i];
+        if(arr[i] > max)
+            max = arr[i];
+
+        sum += arr[i];
+    }
+
+    average = sum / 5.0;
+
+    printf("Minimum: %d\n", min);
+    printf("Maximum: %d\n", max);
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
 
