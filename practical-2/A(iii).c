403a7b543bee6dd0cb1868afa5719b4ae55be0b5
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:26:59 +0530

    Update A(iii).c

--- practical-2/A(iii).c
+++ practical-2/A(iii).c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[2][2][2];
+    int i, j, k;
+
+    // Taking input from user
+    printf("Enter 8 elements for a 2x2x2 3D array:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            for(k = 0; k < 2; k++) {
+                printf("Element [%d][%d][%d]: ", i, j, k);
+                scanf("%d", &arr[i][j][k]);
+            }
+        }
+    }
+
+    // Printing the array elements
+    printf("\n3D Array elements are:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            for(k = 0; k < 2; k++) {
+                printf("arr[%d][%d][%d] = %d\n", i, j, k, arr[i][j][k]);
+            }
+        }
+    }
+
+    return 0;
+}
 
