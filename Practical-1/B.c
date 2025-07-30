1f86be86cffc223ce99b69b8579023cec33d6a98
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:16:54 +0530

    Update and rename b.c to B.c

---
+++ Practical-1/B.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
+    else
+        return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    int n;
+    printf("Enter the position (n) to find Fibonacci number: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number at position %d is %d\n", n, fibonacci(n));
+
+    return 0;
+}
+
