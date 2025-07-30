7e08417bb8f70c69145129aa5bafedcaff887d59
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:18:52 +0530

    Update E.c

--- Practical-1/E.c
+++ Practical-1/E.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, sum;
+    int *p1 = &a, *p2 = &b;
+
+    sum = *p1 + *p2;
+
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
 
