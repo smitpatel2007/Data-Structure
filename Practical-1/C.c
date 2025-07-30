3513e2c406ef04634e03867b319e9c02f61c631a
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:17:39 +0530

    Update C.c

--- Practical-1/C.c
+++ Practical-1/C.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+int main() {
+    int num = 25;
+    int *ptr;
+
+    ptr = &num;
+
+    printf("Value of num: %d\n", num);
+    printf("Address of num: %p\n", ptr);
+
+    return 0;
+}
